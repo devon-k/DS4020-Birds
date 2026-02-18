b5d1ec3b820864895d9937337f91cb09bf113565
dkatragadda1 <devonk@iastate.edu>
2026-02-17 18:18:14 -0600

    write to csv

--- scripts/birdnetlib_basics.py
+++ scripts/birdnetlib_basics.py
@@ -1,6 +1,6 @@
 from birdnetlib import Recording
 from birdnetlib.analyzer import Analyzer
-import json
+import csv
 import time
 import datetime
 import os
@@ -15,7 +15,7 @@ DEFAULT_AUDIO = "DMWW-EXP_0+1_20200411_062600.wav"
 
 # Full paths used by the script (edit these variables if desired)
 file_path = os.path.join(INPUTS_DIR, DEFAULT_AUDIO)
-out_path = os.path.join(OUTPUTS_DIR, os.path.splitext(DEFAULT_AUDIO)[0] + ".json")
+out_path = os.path.join(OUTPUTS_DIR, os.path.splitext(DEFAULT_AUDIO)[0] + ".csv")
 
 os.makedirs(OUTPUTS_DIR, exist_ok=True)
 
@@ -26,10 +26,12 @@ try:
 
     analyzer_start = time.perf_counter()
 
+    #TODO - get longitude and latitude from file
+    #TODO - get date from file
     recording = Recording(
         analyzer,
         file_path,
-        min_conf=0.80,
+        min_conf=0.10,
         lat=42.0347,
         lon=-93.6199,
         date=datetime.datetime(year=2015, month=5, day=20),
@@ -39,8 +41,15 @@ try:
     end1 = time.perf_counter()
     print(f"Analysis complete: {end1-analyzer_start}")
 
-    with open(out_path, "w") as file:
-        json.dump(recording.detections, file, indent=4)
+    with open(out_path, "w", newline="") as csvfile:
+        if recording.detections:
+            fieldnames = recording.detections[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(recording.detections)
+            print(f"Saved {len(recording.detections)} detections to {out_path}")
+        else:
+            print("No detections found.")
 
 finally:
     end = time.perf_counter()
