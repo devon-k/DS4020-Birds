2be2fa21584187d5640ad217e51321dbd6930e86
Nick DeReus <nadereus@iastate.edu>
2026-02-19 13:05:11 -0600

    Adding TODO's

--- scripts/copy_inputs.py
+++ scripts/copy_inputs.py
@@ -5,12 +5,15 @@ ROOT = Path("z://").resolve()
 LAB_DIRECTORY = ROOT / "ARU_data"
 DESTINATION = Path("inputs")
 
+#TODO - Add functions to make it easier to get your own pathing w/o hard coding it
+
 def get_bird_file_paths(root_directory = LAB_DIRECTORY):
     """ Generates a collection of paths which contains all 
     the files inside the location-coded folders of the ARU_data.
 
     You'll need to create the proper root directory path for your own computer.
     """
+
     return root_directory.glob("???/*/*")
 
 def copy_bird_audio(paths, destination = DESTINATION, num_files = -1):
@@ -18,7 +21,9 @@ def copy_bird_audio(paths, destination = DESTINATION, num_files = -1):
 
     Collects all files if num_files is set to -1.
     """
-    
+
+    #TODO - Add necessary information to filenames OR export an index which contains it.
+
     for i, path in enumerate(paths):
         if i == num_files:
             break
