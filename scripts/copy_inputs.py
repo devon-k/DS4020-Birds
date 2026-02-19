83711b28ba6f2e3e3a9068316f967ad11e0c1db4
Nick DeReus <nadereus@iastate.edu>
2026-02-19 15:49:22 -0600

    Made functions more flexible
    copy_bird_audio can now take single paths as well as collections of paths
    
    get_bird_file_paths now takes the actual lab directory as a root instead of the ARU folder

--- scripts/copy_inputs.py
+++ scripts/copy_inputs.py
@@ -2,33 +2,47 @@ from pathlib import Path
 from shutil import copy
 
 ROOT = Path("z://").resolve()
-LAB_DIRECTORY = ROOT / "ARU_data"
 DESTINATION = Path("inputs")
 
-#TODO - Add functions to make it easier to get your own pathing w/o hard coding it
-
-def get_bird_file_paths(root_directory = LAB_DIRECTORY):
-    """ Generates a collection of paths which contains all 
+def get_bird_file_paths(root_directory = ROOT):
+    """ Produces a generator (stream) of paths which contains all 
     the files inside the location-coded folders of the ARU_data.
 
-    You'll need to create the proper root directory path for your own computer.
+    Just needs the path to the network folder containing the data.
+
+    The generator currently includes a small number (361) files which aren't audio..
+    I don't think there is a good way around that so it has to be handled by the client taking accepting the generator.
     """
 
-    return root_directory.glob("???/*/*")
+    # Takes either a string or path, needs to convert str to path.
+    if type(root_directory) == str:
+        root_directory = Path(root_directory).resolve()
+
+    lab_directory = root_directory / "ARU_data"
+    return lab_directory.glob("???/*/*")
+
+#TODO - Add filtering options, maybe random selection.
 
 def copy_bird_audio(paths, destination = DESTINATION, num_files = -1):
     """ Copies a number of files from a collection of paths to a destination folder.
 
+    paths can be a path, string, generator, or collection.
     Collects all files if num_files is set to -1.
     """
 
     #TODO - Add necessary information to filenames OR export an index which contains it.
 
-    for i, path in enumerate(paths):
-        if i == num_files:
-            break
-        copy(path, DESTINATION)
+    # Handle single paths/strings
+    if type(paths) == Path or type(paths) == str:
+        if ".wav" in str(path) or ".flac" in str(path):
+            copy(path, DESTINATION)
+    # Handle collections/generators
+    else:
+        for i, path in enumerate(paths):
+            if i == num_files:
+                break
+            if ".wav" in str(path) or ".flac" in str(path):
+                copy(path, DESTINATION)
 
 if __name__ == "__main__":
-    file_paths = get_bird_file_paths()
-    copy_bird_audio(file_paths, num_files=1)
+    copy_bird_audio(file_paths, num_files=1)    file_paths = get_bird_file_paths("z://")
