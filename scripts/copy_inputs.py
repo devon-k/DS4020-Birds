a747e79418bd7b5eae530248026695132d44f949
Nick DeReus <nadereus@iastate.edu>
2026-02-19 13:00:28 -0600

    Added some documentation, one minor bugfix

--- scripts/copy_inputs.py
+++ scripts/copy_inputs.py
@@ -6,9 +6,18 @@ LAB_DIRECTORY = ROOT / "ARU_data"
 DESTINATION = Path("inputs")
 
 def get_bird_file_paths(root_directory = LAB_DIRECTORY):
+    """ Generates a collection of paths which contains all 
+    the files inside the location-coded folders of the ARU_data.
+
+    You'll need to create the proper root directory path for your own computer.
+    """
     return root_directory.glob("???/*/*")
 
 def copy_bird_audio(paths, destination = DESTINATION, num_files = -1):
+    """ Copies a number of files from a collection of paths to a destination folder.
+
+    Collects all files if num_files is set to -1.
+    """
     
     for i, path in enumerate(paths):
         if i == num_files:
@@ -16,5 +25,5 @@ def copy_bird_audio(paths, destination = DESTINATION, num_files = -1):
         copy(path, DESTINATION)
 
 if __name__ == "__main__":
-    file_paths = get_bird_file_paths
+    file_paths = get_bird_file_paths()
     copy_bird_audio(file_paths, num_files=1)
